9089088131a71ddbd21fa962c54ebf61e060d18d
user <user@secure168>
2016-01-12 10:19:53 +0700

    initial

---
+++ src/com/egat.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author user
+ */
+public class egat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        String msg1 = "";
+        System.out.println(msg1);
+    }
+    
+}
